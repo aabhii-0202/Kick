2e9d6eba3c977376c1253eeea99860ed0f3724df
Abhishek Kumar Singh <abhisheksingh1911a5@gmail.com>
2022-05-24 12:51:05 +0530

    Splash Screen done

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Text, Input, View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { StackActions, NavigationActions } from 'react-navigation';
+import SearchBar from '../Components/Common/SearchBar';
 
 const HomeScreen = ({navigation}) => {
 
      return (
-        <View>
-
+        <View style={styles.view}>
+            <SearchBar
+                placeholder='Search your country'
+            />
+            <SearchBar
+                placeholder='Search your country'
+            />
         </View>
     );
 };
@@ -15,7 +21,11 @@ const HomeScreen = ({navigation}) => {
 
 
 const styles = StyleSheet.create({
-
+    view:{
+        backgroundColor: '000000',
+        height: 200,
+        width: 200
+    }
 });
 
 
--- src/Screens/SplashScreen.js
+++ src/Screens/SplashScreen.js
@@ -2,31 +2,52 @@ import React from 'react';
 import {View, Text }from 'react-native';
 import Button from '../Components/Common/Button';
 import { CommonActions } from '@react-navigation/native';
+import CardSection from '../Components/Common/CardSection';
+import LottieView from 'lottie-react-native';
 
 
 const SplashScreen = ({navigation}) => {
+  
    return (
     <View>
-      <Text
-        style={{marginVertical:200,marginLeft:160}}>Splash Screen
-      </Text>
-      <Button
-        onPress={ () =>{
-          navigation.dispatch(
-            CommonActions.reset({
-              index: 0,
-              routes: [
-                { name: 'Home' }
-              ],
-            })
-          );
-        }
-        }
-      >Kick It!</Button>
-
+      <LottieView 
+        source={require('../../Assets/images/splash.json')} 
+        size={50} 
+        autoPlay 
+        loop 
+        resizeMode="cover" 
+        style={styles.image} />
+      <CardSection style={styles.btn}>
+        <Button 
+          onPress={ () =>{
+            navigation.dispatch(
+              CommonActions.reset({
+                index: 0,
+                routes: [
+                  { name: 'Home' }
+                ],
+              })
+            );
+          }
+          }
+        >Kick It!</Button>
+      </CardSection>
     </View>
   );
 };
+
+const styles = {
+  image:{
+    width: 300, 
+    aspectRatio: 300 / 500,
+    flexGrow: 1, 
+    alignSelf: 'center',
+  },
+  btn:{
+    justifyContent: 'flex-end',
+    width: '100%',
+  }
+}
 
 
 
