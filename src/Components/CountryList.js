d69c42fa4641fe331ce46dd0770bc679ce341430
Abhishek Kumar Singh <abhisheksingh1911a5@gmail.com>
2022-05-24 16:43:53 +0530

    Countries component made

---
+++ src/Components/CountryList.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View,Text ,StyleSheet,TouchableOpacity } from 'react-native';
+import { SvgCssUri } from 'react-native-svg';
+import {Colors} from '../Components/Common/Colors'
+
+
+const App = ({url,name}) => {
+
+     return (
+         
+        <TouchableOpacity
+            style={styles.touchable}
+        >
+            <View style={styles.image}>
+            <SvgCssUri
+                height='100%'
+                width='100%'
+                uri={url}
+                style={styles.flag}
+            />
+            </View>
+            <Text 
+                style={styles.text}
+            >{name}</Text>
+        </TouchableOpacity>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    flag:{
+        
+    },
+    text:{
+        color: 'black',
+        fontSize: 20,
+        flex:1,
+        textAlign: 'center',
+        fontWeight:'bold',
+        
+    },
+    image:{
+        backgroundColor:'white',
+        flex:4,
+        backgroundColor: Colors.mybackground,
+
+
+    },touchable:{
+        width:'40%',
+        height:150,
+        marginHorizontal:16,
+        marginVertical: 8,
+        padding:4,
+        backgroundColor: Colors.mybackground
+    },
+});
+
+
+export default App;
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { Text, Input, View, StyleSheet } from 'react-native';
+import { Text, Input, View, StyleSheet,Button } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { StackActions, NavigationActions } from 'react-navigation';
 import SearchBar from '../Components/Common/SearchBar';
+import CountryList from '../Components/CountryList';
+import {Colors} from '../Components/Common/Colors'
+import football from '../Api/football';
 
 const HomeScreen = ({navigation}) => {
 
+    const searchApi = async () => {
+        const response = football.get('/v3/players',{
+          params: {
+            league: '61',
+            search: 'neymar'
+          }
+        });
+        
+        response.then(()=>{
+            console.log({response});
+        })
+      }
+
      return (
         <View style={styles.view}>
-            <SearchBar
+            {/* <SearchBar
                 placeholder='Search your country'
+            /> */}
+            <CountryList
+                name="France"
+                url="https://media.api-sports.io/flags/fr.svg"
             />
-            <SearchBar
-                placeholder='Search your country'
+             <CountryList
+                name="Spain"
+                url="https://media.api-sports.io/flags/es.svg"
             />
+            {/* <Button
+                title='press'
+                onPress={searchApi}
+            /> */}
         </View>
     );
 };
@@ -22,9 +47,7 @@ const HomeScreen = ({navigation}) => {
 
 const styles = StyleSheet.create({
     view:{
-        backgroundColor: '000000',
-        height: 200,
-        width: 200
+        backgroundColor: Colors.mybackground,
     }
 });
 
